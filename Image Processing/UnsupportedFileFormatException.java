144fa25094805a4387f4139d43a3fe49259fe547
AlexStolt <stalexandros@uth.gr>
2021-04-20 19:43:40 +0300

    Skip Warning

--- Image Processing/UnsupportedFileFormatException.java
+++ Image Processing/UnsupportedFileFormatException.java
@@ -1,6 +1,7 @@
 package ce326.hw2;
 
 public class UnsupportedFileFormatException extends java.lang.Exception {
+    static final long serialVersionUID = -4567891456L;
     public UnsupportedFileFormatException(){
 
     }
